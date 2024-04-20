bf45db0f9bc6d1382e8c0c237486ea84b6c611c0
antoniapr <ad.protopsalti@gmail.com>
2024-04-20 12:14:22 +0300

    changes

--- src/main/java/com/team1/VaccinationProject/VaccinationProjectApplication.java
+++ src/main/java/com/team1/VaccinationProject/VaccinationProjectApplication.java
@@ -2,12 +2,27 @@ package com.team1.VaccinationProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/*  Error - debug:
+* Looks like the initial problem is with the auto-config.
+* If you don't need the datasource, simply remove it from the auto-config process:
+*/
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class VaccinationProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaccinationProjectApplication.class, args);
+
+
+
+
+
 	}
+
+
+
+
+
 
 }
